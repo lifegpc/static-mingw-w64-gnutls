b8c31064fa1ddc26f16dfcefc1ea0d5d7556b731
lifegpc <g1710431395@gmail.com>
2021-05-12 11:13:54 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cd packages/mingw-w64-gnutls || return 1
 git apply ../../mingw-w64-gnutls.patch || return 1
-MINGW_ARCH=mingw64 makepkg-mingw -sLf || return 1
+MINGW_ARCH=mingw64 makepkg-mingw -sLf --nosign --noconfirm --skippgpcheck --noprogressbar || return 1
