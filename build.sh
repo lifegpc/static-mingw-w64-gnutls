e9035d9c87270d4ad34ac498d6ef390b870ee6a5
lifegpc <g1710431395@gmail.com>
2021-05-12 23:33:42 +0800

    add skipchecksums

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ cd packages || exit 1
 git apply --ignore-space-change --ignore-whitespace '../mingw-w64-gnutls.patch' || exit 1
 cd mingw-w64-gnutls || exit 1
 dos2unix PKGBUILD || exit 1
-MINGW_ARCH=mingw64 makepkg-mingw -sLf --nosign --noconfirm --skippgpcheck --noprogressbar || exit 1
+MINGW_ARCH=mingw64 makepkg-mingw -sLf --nosign --noconfirm --skippgpcheck --noprogressbar --skipchecksums || exit 1
