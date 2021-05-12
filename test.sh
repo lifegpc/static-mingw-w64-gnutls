3c493e14da1fca1095bdde6cb258b45ded9534ac
lifegpc <g1710431395@gmail.com>
2021-05-12 18:36:57 +0800

    fix wrong path

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-cd packages/mingw-w64-gnutls
+cd packages/mingw-w64-gnutls/src/build*
 make install
-cd ../../
+cd ../../../../
 s = `pkg-config --static --libs gnutls`
 gcc test.c -o test.o -static $s
