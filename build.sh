4006c4d61f4ae3ee1cc493345cacbe2668e47411
lifegpc <41434272+lifegpc@users.noreply.github.com>
2021-05-12 11:46:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd packages || exit 1
-git apply --reject --whitespace=fix '../mingw-w64-gnutls.patch' || exit 1
+git apply --ignore-space-change --ignore-whitespace '../mingw-w64-gnutls.patch' || exit 1
 cd mingw-w64-gnutls || exit 1
 MINGW_ARCH=mingw64 makepkg-mingw -sLf --nosign --noconfirm --skippgpcheck --noprogressbar || exit 1
