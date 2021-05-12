b70e406fec3ee48c5cbf495780f9334b50a5b01b
lifegpc <g1710431395@gmail.com>
2021-05-12 19:18:19 +0800

    fix bug in test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 cd packages/mingw-w64-gnutls/src/build*
 make install
 cd ../../../../
-s = `pkg-config --static --libs gnutls`
+s=`pkg-config --static --libs gnutls`
+echo $s
 gcc test.c -o test.o -static $s
