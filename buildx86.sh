2fcb6d79c9f091ee9c240c7bb26b65eae893a09e
lifegpc <g1710431395@gmail.com>
2021-05-13 15:26:28 +0800

    add x86 build

---
+++ buildx86.sh
@@ -0,0 +1,6 @@
+cp -v 0006-iconv-add-it-to-pkg-config.patch packages/mingw-w64-gnutls || exit 1
+cd packages || exit 1
+git apply --ignore-space-change --ignore-whitespace '../mingw-w64-gnutls.patch' || exit 1
+cd mingw-w64-gnutls || exit 1
+dos2unix PKGBUILD || exit 1
+MINGW_ARCH=mingw32 makepkg-mingw -sLf --nosign --noconfirm --skippgpcheck --noprogressbar --skipchecksums || exit 1
