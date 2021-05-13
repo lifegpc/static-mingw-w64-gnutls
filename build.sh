44deff7167e0cdb6a5eb08ab5b392a1332af1a68
lifegpc <g1710431395@gmail.com>
2021-05-13 08:03:49 +0800

    try to fix wrong patch

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+cp -v 0006-iconv-add-it-to-pkg-config.patch packages/mingw-w64-gnutls || exit 1
 cd packages || exit 1
 git apply --ignore-space-change --ignore-whitespace '../mingw-w64-gnutls.patch' || exit 1
 cd mingw-w64-gnutls || exit 1
